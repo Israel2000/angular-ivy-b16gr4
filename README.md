3688335fb92fb97e0803de32c7c6a35cb28a7026
Israel2000 <ibelete2000@gmail.com>
2022-06-24 23:04:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-b16gr4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-b16gr4)
---
+++ src/app/svg.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-svg',
+  template: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="" viewBox="0 0 1245 454">
+  <g id="Rectangle_12" data-name="Rectangle 12" transform="translate(216 116)" stroke="#707070" stroke-width="1">
+    <rect width="885" height="207" stroke="none"/>
+    <rect x="0.5" y="0.5" width="884" height="206" fill="none"/>
+  </g>
+  <text id="Holy_Conf." data-name="Holy Conf." transform="translate(334 274)" fill="#fff" font-size="150" font-family="HelveticaNeue-LightItalic, Helvetica Neue" font-weight="300" font-style="italic"><tspan x="0" y="0">Holy Conf.</tspan></text>
+</svg>
+
+`,
+})
+export class SvgComponent  {}
